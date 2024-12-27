74fe5b02c2a54eaf83170fb593dbb74d4823751d
maruf-pfc <mdmarufsarker.mms@gmail.com>
2024-12-28 03:07:38 +0600

    add: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/footer";
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -43,6 +44,7 @@ export default function RootLayout({
       <body className={`${inter.className} h-full`}>
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
